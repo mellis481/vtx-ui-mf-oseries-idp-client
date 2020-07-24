004b9405b01b95500284ef08072446f33a31e591
Michael Ellis <michael.ellis@Vertexinc.Com>
2020-07-24 17:59:59 -0400

    Fixed

--- webpack.config.js
+++ webpack.config.js
@@ -9,15 +9,23 @@ module.exports = (webpackConfigEnv) => {
     webpackConfigEnv,
   });
 
-  return {
-    ...defaultConfig,
-    entry: path.resolve(process.cwd(), `src/vtx-ui-mf-oseries-idp-client.tsx`),
-    output: {
-      filename: `vtx-ui-mf-oseries-idp-client.js`,
-      libraryTarget: "system",
-      path: path.resolve(process.cwd(), "dist"),
-      jsonpFunction: `webpackJsonp_vtx-ui-mf-oseries-idp-client`,
+  return webpackMerge.smart(
+    {
+      ...defaultConfig,
+      entry: path.resolve(
+        process.cwd(),
+        `src/vtx-ui-mf-oseries-idp-client.tsx`
+      ),
+      output: {
+        filename: `vtx-ui-mf-oseries-idp-client.js`,
+        libraryTarget: "system",
+        path: path.resolve(process.cwd(), "dist"),
+        jsonpFunction: `webpackJsonp_vtx-ui-mf-oseries-idp-client`,
+      },
+      externals: ["single-spa"],
     },
-    externals: ["single-spa"],
-  };
+    {
+      // modify the webpack config however you'd like to by adding to this object
+    }
+  );
 };
