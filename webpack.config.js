5970774f78e52ef8aebd46121bee74bb5dd0d05b
Michael Ellis <michael.ellis@Vertexinc.Com>
2020-07-24 17:46:55 -0400

    Updated MFE name

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const webpackMerge = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 
@@ -8,10 +9,15 @@ module.exports = (webpackConfigEnv) => {
     webpackConfigEnv,
   });
 
-  return webpackMerge.smart(
-    { ...defaultConfig, externals: ["single-spa"] },
-    {
-      // modify the webpack config however you'd like to by adding to this object
-    }
-  );
+  return {
+    ...defaultConfig,
+    entry: path.resolve(process.cwd(), `src/vtx-ui-mf-oseries-idp-client.tsx`),
+    output: {
+      filename: `vtx-ui-mf-oseries-idp-client.js`,
+      libraryTarget: "system",
+      path: path.resolve(process.cwd(), "dist"),
+      jsonpFunction: `webpackJsonp_vtx-ui-mf-oseries-idp-client`,
+    },
+    externals: ["single-spa"],
+  };
 };
